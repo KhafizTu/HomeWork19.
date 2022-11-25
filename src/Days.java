a4e82fe75d992da46458c91bdae14965b8032352
Khafiz <khafizturusbekuulu0@gmail.com>
2022-11-25 13:31:03 +0600

    Commit

--- src/Days.java
+++ src/Days.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public enum Days {
+public enum Days {
+
+    MONDAY("Дуйшомбу куну Java окуйм"),
+    TUESDAY("Шейшемби күнү English сабагын окуйм"),
+    WEDNESDAY("Шаршенби куну Talking club болот"),
+    THURSDAY("Бейшемби куну Practice Lesson окуйм"),
+    FRIDAY("Жума куну Event жана  Technical Lesson болот"),
+    SATURDAY("Ишемби куну  ДЕМ АЛЫШ"),
+    SUNDAY("Жекшемби куну ДЕМ АЛЫШ");
+    private String raspisanie;
+
+    Days(String raspisanie){
+        this.raspisanie=raspisanie;
+    }
+    public String getRaspisanie(){
+        return raspisanie;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        String day = scanner.nextLine();
+
+
+        if (day.equals("Дуйшомбу")) {
+            Days days = Days.MONDAY;
+            System.out.println(days.getRaspisanie());
+        } else if (day.equals("Шейшемби")) {
+            Days days = Days.TUESDAY;
+            System.out.println(days.getRaspisanie());
+        } else if (day.equals("Шаршенби")) {
+            Days days = Days.WEDNESDAY;
+            System.out.println(days.getRaspisanie());
+        } else if (day.equals("Бейшемби")) {
+            Days days = Days.THURSDAY;
+            System.out.println(days.getRaspisanie());
+        } else if (day.equals("Жума")) {
+            Days days = Days.FRIDAY;
+            System.out.println(days.getRaspisanie());
+        } else if (day.equals("Ишемби")) {
+            Days days = Days.SATURDAY;
+            System.out.println(days.getRaspisanie());
+        } else if (day.equals("Жекшемби")) {
+            Days days = Days.SUNDAY;
+            System.out.println(days.getRaspisanie());
+        } else if (day.equals("Pacписание")) {
+            Days days1 = Days.MONDAY;
+            System.out.println(days1.getRaspisanie());
+            Days days2 = Days.TUESDAY;
+            System.out.println(days2.getRaspisanie());
+            Days days3 = Days.WEDNESDAY;
+            System.out.println(days3.getRaspisanie());
+            Days days4 = Days.THURSDAY;
+            System.out.println(days4.getRaspisanie());
+            Days days5 = Days.FRIDAY;
+            System.out.println(days5.getRaspisanie());
+            Days days6 = Days.SATURDAY;
+            System.out.println(days6.getRaspisanie());
+            Days days7 = Days.SUNDAY;
+            System.out.println(days7.getRaspisanie());
+        }
+
+
     }
 }
